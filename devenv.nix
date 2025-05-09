6a2b5d71a8cbf63a098afc14642b4f88f9bdd884
VJB22 <barocsaiviki22@gmail.com>
2025-05-09 16:57:57 +0200

    Initial Load

---
+++ devenv.nix
@@ -0,0 +1,61 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "PCAP analysis";
+
+  # https://devenv.sh/packages/
+  packages = with pkgs; [
+    duckdb
+  ];
+
+  cachix.enable = false;
+
+  # https://devenv.sh/languages/
+  languages.python = {
+    enable = true;
+    venv.enable = true;
+    venv.requirements = ''
+      requests
+      jupyterlab
+      notebook
+      numpy
+      pandas
+      pipx
+    '';
+    uv.enable = true;
+  };
+
+  # https://devenv.sh/processes/
+  # processes.cargo-watch.exec = "cargo-watch";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = ''
+    Starting $GREET
+  '';
+
+  enterShell = ''
+    hello
+    git --version
+  '';
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep --color=auto "${pkgs.git.version}"
+  '';
+
+  # https://devenv.sh/git-hooks/
+  # git-hooks.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
